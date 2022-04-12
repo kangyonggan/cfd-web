131de2fe4c62077d3cbf900942ee9f4194d07cff
kangyonggan <java@kangyonggan.com>
2022-04-12 13:22:01 +0800

    fund fee

--- src/views/wallet/contract.vue
+++ src/views/wallet/contract.vue
@@ -8,8 +8,8 @@
           {{ NumberUtil.format((account.totalAmount + orderAmountInfo.unsettleProfit)) }} USDT
 
           <el-button
-            type="primary"
             style="float: right"
+            plain
             @click="$refs['transfer-modal'].show()"
           >
             划转
@@ -58,101 +58,176 @@
         </div>
       </div>
 
-      <div class="position-title">
-        仓位
-      </div>
-      <el-table
-        :data="orderHeldList"
-        style="width: 100%"
+      <el-tabs
+        v-model="activeTab"
+        class="tabs"
+        @tab-click="changeTab"
       >
-        <el-table-column
-          prop="quotationCoin"
-          label="交易对"
-          min-width="100"
+        <el-tab-pane
+          label="当前持仓"
+          name="orderHeld"
         >
-          <template #default="scope">
-            <div
-              style="cursor: pointer"
-              @click="changeSymbol(scope.row.quotationCoin + scope.row.marginCoin)"
+          <el-table
+            :data="orderHeldList"
+            style="width: 100%"
+          >
+            <el-table-column
+              prop="quotationCoin"
+              label="交易对"
+              min-width="100"
             >
-              <span style="color: var(--el-color-primary)">
-                {{ scope.row.quotationCoin }}
-              </span>
-              <span style="font-size: 12px;color: var(--app-text-color-dark)">/{{ scope.row.marginCoin }}
-              </span>
-            </div>
-          </template>
-        </el-table-column>
-        <el-table-column
-          prop="positionSide"
-          label="方向"
-          min-width="100"
-        >
-          <template #default="scope">
-            <span :class="scope.row.positionSide === 'LONG' ? 'bullish' : 'bearish'">
-              {{ scope.row.positionSide === 'LONG' ? '做多' : '做空' }}({{ scope.row.leverage }}x)
-            </span>
-          </template>
-        </el-table-column>
-        <el-table-column
-          prop="margin"
-          label="保证金"
-          min-width="110"
-        />
-        <el-table-column
-          prop="openPrice"
-          label="开仓价格"
-          min-width="100"
-        >
-          <template #default="scope">
-            {{ scope.row.openPrice }}
-          </template>
-        </el-table-column>
-        <el-table-column
-          prop="lastPrice"
-          label="最新价格"
-          min-width="100"
-        >
-          <template #default="scope">
-            <span v-if="scope.row.lastPrice !== undefined">
-              {{ scope.row.lastPrice }}
-            </span>
-            <span v-else>
-              --
-            </span>
-          </template>
-        </el-table-column>
-        <el-table-column
-          prop="unsettleProfit"
-          label="未实现盈亏"
-          min-width="170"
-        >
-          <template #default="scope">
-            <span
-              :class="scope.row.profitClass"
-              v-if="scope.row.profit"
+              <template #default="scope">
+                <div
+                  style="cursor: pointer"
+                  @click="changeSymbol(scope.row.quotationCoin + scope.row.marginCoin)"
+                >
+                  <span style="color: var(--el-color-primary)">
+                    {{ scope.row.quotationCoin }}
+                  </span>
+                  <span style="font-size: 12px;color: var(--app-text-color-dark)">/{{ scope.row.marginCoin }}
+                  </span>
+                </div>
+              </template>
+            </el-table-column>
+            <el-table-column
+              prop="positionSide"
+              label="方向"
+              min-width="100"
             >
-              {{ scope.row.addIcon }}{{ NumberUtil.formatUsdt(scope.row.profit) }}({{ scope.row.profitRate }})
-            </span>
-            <span v-else>
-              --
-            </span>
-          </template>
-        </el-table-column>
-        <el-table-column
-          label="强平价格"
-          min-width="110"
+              <template #default="scope">
+                <span :class="scope.row.positionSide === 'LONG' ? 'bullish' : 'bearish'">
+                  {{ scope.row.positionSide === 'LONG' ? '做多' : '做空' }}({{ scope.row.leverage }}x)
+                </span>
+              </template>
+            </el-table-column>
+            <el-table-column
+              prop="margin"
+              label="保证金"
+              min-width="110"
+            />
+            <el-table-column
+              prop="openPrice"
+              label="开仓价格"
+              min-width="100"
+            >
+              <template #default="scope">
+                {{ scope.row.openPrice }}
+              </template>
+            </el-table-column>
+            <el-table-column
+              prop="lastPrice"
+              label="最新价格"
+              min-width="100"
+            >
+              <template #default="scope">
+                <span v-if="scope.row.lastPrice !== undefined">
+                  {{ scope.row.lastPrice }}
+                </span>
+                <span v-else>
+                  --
+                </span>
+              </template>
+            </el-table-column>
+            <el-table-column
+              prop="unsettleProfit"
+              label="未实现盈亏"
+              min-width="170"
+            >
+              <template #default="scope">
+                <span
+                  :class="scope.row.profitClass"
+                  v-if="scope.row.profit"
+                >
+                  {{ scope.row.addIcon }}{{ NumberUtil.formatUsdt(scope.row.profit) }}({{ scope.row.profitRate }})
+                </span>
+                <span v-else>
+                  --
+                </span>
+              </template>
+            </el-table-column>
+            <el-table-column
+              label="强平价格"
+              min-width="110"
+            >
+              <template #default="scope">
+                <span v-if="scope.row.forceClosePrice !== undefined">
+                  {{ scope.row.forceClosePrice }}
+                </span>
+                <span v-else>
+                  --
+                </span>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+        <el-tab-pane
+          label="资金流水"
+          name="accountLog"
         >
-          <template #default="scope">
-            <span v-if="scope.row.forceClosePrice !== undefined">
-              {{ scope.row.forceClosePrice }}
-            </span>
-            <span v-else>
-              --
-            </span>
-          </template>
-        </el-table-column>
-      </el-table>
+          <ul
+            class="log-list"
+            v-loading="loadingAccountLog"
+          >
+            <li>
+              <div>
+                币种
+              </div>
+              <div>
+                类型
+              </div>
+              <div
+                class="large"
+                style="text-align: left"
+              >
+                流水号
+              </div>
+              <div class="large">
+                金额
+              </div>
+              <div class="large">
+                时间
+              </div>
+            </li>
+            <li v-if="accountLogList.length === 0">
+              <div style="text-align: center;width: 100%;color: var(--app-text-color-dark);font-size: 13px;">
+                暂无数据
+              </div>
+            </li>
+            <li
+              v-for="item in accountLogList"
+              :key="item.id"
+            >
+              <div>
+                {{ item.currency }}
+              </div>
+              <div>
+                {{ getTransferType(item.type) }}
+              </div>
+              <div
+                class="large"
+                style="text-align: left"
+              >
+                {{ item.serialNo }}
+              </div>
+              <div class="large">
+                {{ item.amount }}
+              </div>
+              <div class="large">
+                {{ DateTimeUtil.format(item.createTime) }}
+              </div>
+            </li>
+          </ul>
+
+          <el-pagination
+            v-show="total > 10"
+            style="margin: 10px 0;float:right"
+            layout="prev, pager, next"
+            @current-change="getAccountLog($event)"
+            :total="total"
+            :current-page="pageNum"
+          />
+        </el-tab-pane>
+      </el-tabs>
     </div>
 
     <transfer-modal
@@ -171,10 +246,51 @@
       return {
         account: {},
         orderAmountInfo: {},
-        orderHeldList: []
+        orderHeldList: [],
+        activeTab: localStorage.getItem('contractActiveTab') || 'orderHeld',
+        accountLogList: [],
+        loadingAccountLog: false,
+        total: 0,
+        pageNum: 1
       }
     },
     methods: {
+      changeTab() {
+        localStorage.setItem('contractActiveTab', this.activeTab)
+        if (this.activeTab === 'accountLog') {
+          this.getAccountLog()
+        }
+      },
+      getTransferType(type) {
+        if (type === 'TRANSFER_IN') {
+          return '划转-转入'
+        } else if (type === 'TRANSFER_OUT') {
+          return '划转-转出'
+        } else if (type === 'CLOSE_PROFIT') {
+          return '平仓收益'
+        } else if (type === 'CLOSE_FEE') {
+          return '平仓手续费'
+        } else if (type === 'FUND_FEE') {
+          return '资金费'
+        }
+
+        return  type
+      },
+      getAccountLog(pageNum) {
+        if (!pageNum) {
+          pageNum = 1
+        }
+        this.pageNum = pageNum
+        this.loadingAccountLog = true
+        this.axios.get('/v1/wallet/accountLog?accountType=CONTRACT&current=' + pageNum).then(data => {
+          this.accountLogList = data.records
+          this.total = data.total
+        }).catch(res => {
+          this.$error(res.msg)
+        }).finally(() => {
+          this.loadingAccountLog = false
+        })
+      },
       changeSymbol(symbol) {
         this.$router.push({
           path: '/contract',
@@ -204,6 +320,7 @@
     },
     mounted() {
       this.getOverview()
+      this.changeTab()
 
       this.$eventBus.on('updateAccount', this.updateAccount)
       this.$eventBus.on('updateOrderAmountInfo', this.updateOrderAmountInfo)
@@ -256,12 +373,35 @@
       }
     }
 
-    .position-title {
+    .tabs {
       margin-top: 40px;
-      font-size: 18px;
-      font-weight: bold;
-      color: var(--app-text-color-light);
       padding-bottom: 10px;
+
+      .log-list {
+        list-style: none;
+        padding-left: 0;
+        margin-top: 0;
+
+        li {
+          height: 35px;
+          border-bottom: 1px solid var(--app-border-color);
+          padding-top: 19px;
+
+          div {
+            display: inline-block;
+            width: 14%;
+          }
+
+          .large {
+            width: 24%;
+            text-align: right;
+          }
+        }
+
+        li:first-child {
+          padding-top: 0;
+        }
+      }
     }
   }
 </style>
