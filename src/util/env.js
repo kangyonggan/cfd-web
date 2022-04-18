e6392d281bf8fb35f4cb1a3863cb10267f93efbf
kangyonggan <java@kangyonggan.com>
2022-04-18 17:41:35 +0800

    x

--- src/util/env.js
+++ src/util/env.js
@@ -3,7 +3,7 @@ let env = {}
 env.wsHost = 'ws://127.0.0.1:9080'
 
 if (process.env.NODE_ENV !== 'development') {
-  env.wsHost = 'wss://kangyonggan.com'
+  env.wsHost = 'wss://kangyonggan.com/cfd-api'
 }
 
 export default env
