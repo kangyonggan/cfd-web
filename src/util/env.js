6cf3c9efa5425b4a8d7bbb0e8e252c579cf709f9
kangyonggan <yonggan.kang@mochain.vip>
2023-09-08 16:25:41 +0800

    x

--- src/util/env.js
+++ src/util/env.js
@@ -3,7 +3,7 @@ let env = {}
 env.wsHost = 'ws://127.0.0.1:9080'
 
 if (process.env.NODE_ENV !== 'development') {
-  env.wsHost = 'ws://127.0.0.1:9080'
+  env.wsHost = 'ws://192.168.124.90:9080'
 }
 
 export default env
